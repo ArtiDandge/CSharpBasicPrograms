5583b74dba84e2f8f2fcffc75596820d7077cd56
ArtiDandge <58045752+ArtiDandge@users.noreply.github.com>
2020-11-27 16:09:24 +0530

    [Arti Dandge] Add: Method to test Year is Leap or not

---
+++ LeapYearCheck.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloWorld
+{
+    class LeapYearCheck
+    {
+        public static void LeapYear()
+        {
+            Console.WriteLine("Enter year: ");
+            int year = Convert.ToInt32(Console.ReadLine());
+            if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
+            {
+                Console.WriteLine(year + " is leap year");
+            }
+            else
+            {
+                Console.WriteLine(year + " is not leap year");
+            }
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -13,7 +13,8 @@ namespace HelloWorld
                 Console.WriteLine("Choose option below");
                 Console.WriteLine("1. Add two numbers");
                 Console.WriteLine("2. Find Square");
-                Console.WriteLine("3. Exit");
+                Console.WriteLine("3. Test Leap year or not");
+                Console.WriteLine("4. Exit");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
@@ -24,6 +25,9 @@ namespace HelloWorld
                         PowerOfNumber.PowerFunction();
                         break;
                     case 3:
+                        LeapYearCheck.LeapYear();
+                        break;
+                    case 4:
                         test = false;
                         Environment.Exit(0);
                         break;
