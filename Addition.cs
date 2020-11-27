86163b6fbf3794be5f0eb81bd4ee21baac727311
ArtiDandge <58045752+ArtiDandge@users.noreply.github.com>
2020-11-27 15:50:34 +0530

    [Arti Dandge] Add: Method to find addition of two number

---
+++ Addition.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloWorld
+{
+    class Addition
+    {
+        public static void Add()
+        {
+            Console.WriteLine("Enter first number");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second number");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+            int result = number1 + number2;
+            Console.WriteLine("Addition of two number " + result);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,7 +7,28 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to C-Sharp basic program");
-            
+            Boolean test = true;
+            do
+            {
+                Console.WriteLine("Choose option below");
+                Console.WriteLine("1. Add two numbers");
+                Console.WriteLine("2. Exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        Addition.Add();
+                        break;
+                    case 2:
+                        test = false;
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Please enter valid option only");
+                        break;
+                }
+            } while (test != false);
+
         }
     }
 }
